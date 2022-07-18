933908d98508f41cda10496599c633588d04cc25
Rafael Posada <rafael.posada@azumo.co>
2022-07-18 03:05:16 -0500

    Added inserts to the init.sql

--- db/init.sql
+++ db/init.sql
@@ -6,3 +6,14 @@ CREATE TABLE public.star_products (
 	CONSTRAINT star_products_pk PRIMARY KEY (id)
 );
 
+INSERT INTO public.star_products
+(name_product, description, price)
+VALUES('product 1', 'test description', 10.02);
+
+INSERT INTO public.star_products
+(name_product, description, price)
+VALUES('product 2', 'test description 2', 101.02);
+
+INSERT INTO public.star_products
+(name_product, description, price)
+VALUES('product 3', 'test description 3', 3333);
